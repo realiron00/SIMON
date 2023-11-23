a592649b7eaea07fb74cb0aa892d5757f8478142
realiron00 <ic1595@naver.com>
2023-11-24 04:16:09 +0900

    2311240416

--- ccode/Simon.c
+++ ccode/Simon.c
@@ -31,18 +31,11 @@ void simon_encrypt(int round, uint8_t* round_keys, uint8_t* x, uint8_t* y)
 //라운드 키 테스트
 int main()
 {
-    uint8_t key[KEY_WORDS][WORD_SIZE] = 
+    uint8_t key[KEY_WORDS] = {0x90041AC92DB746E1, 0x91875EF010371D99};
+    uint8_t round_keys[NUM_ROUNDS] = {0, };
+    simon_key_schedule(key, round_keys);
+    for(int i = 0; i < NUM_ROUNDS; i++)
     {
-        {
-            0x80, 0x79, 0x78, 0x77, 0x76, 0x75, 0x74,
-            0x73, 0x72, 0x71, 0x70, 0x6F, 0x6E, 0x6D,
-            0x6C, 0x6B
-        }, 
-        {
-            0x60, 0x69, 0x68, 0x67, 0x66, 0x65, 0x64,
-            0x63, 0x62, 0x61, 0x60, 0x5F, 0x5E, 0x5D,
-            0x5C, 0x5B
-        }
-    };
-    uint8_t round_keys[NUM_ROUNDS][WORD_SIZE] = {0, };
+        printf("%d ", round_keys[i]);
+    }
 }
