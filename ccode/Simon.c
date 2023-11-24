651684b7fff83c39cf18e87343db3ee034a84d7e
realiron00 <ic1595@naver.com>
2023-11-25 05:36:41 +0900

    2311250536

--- ccode/Simon.c
+++ ccode/Simon.c
@@ -25,35 +25,11 @@ void simon_key_schedule(uint64_t* key, uint64_t* round_keys)
     }
 }
 
-void simon_key_schedule(uint8_t key[16], uint8_t round_keys[NUM_ROUNDS][16])
+void simon_encrypt(int round, uint64_t* round_keys, uint64_t* x, uint64_t* y)
 {
-    for(int i = 0; i < 2; i++)
-    {
-        round_keys[0][i] = key[i];
-    }
-
-    uint32_t temp_1 = 0;
-    uint32_t temp_2 = 0;
-
-    for(int i = 1; i < NUM_ROUNDS; i++)
-    {
-        // (S: shift left, ⊕: bitwise XOR, mod: modulo)
-        // tmp ← S^−3round_keys[i-1]
-        // tmp ← tmp ⊕ S^−1tmp
-        // round_keys[i] ← ~round_keys[i-2] ⊕ tmp ⊕ z[(i-m) mod 62] ⊕ 3
-        
-        //temp_1에 round_keys[i-1]을 3비트 오른쪽으로 shift한 값을 저장
-        for(int j = 0; j < 4; j++)
-        {
-            temp_1 = temp_1 << 8;
-            temp_1 = temp_1 | round_keys[i-1][j];
-        }
-    }
-}
-
-void simon_encrypt(int round, uint8_t* round_keys, uint8_t* x, uint8_t* y)
-{
-    
+    uint64_t temp = *x;
+    *x = *y ^ (((*x << 1) & (*x << 8)) ^ (*x << 2)) ^ round_keys[round];
+    *y = temp;
 }
 
 //라운드 키 테스트
@@ -68,6 +44,8 @@ int main()
         printf("%016llx\n", round_keys[i]);
     }
     uint64_t plain[2] = {0x6373656420737265, 0x6c6c657661727420};
+    printf("plain:\n");
+    printf("%016llx %016llx\n", plain[0], plain[1]);
     printf("encrypt:\n");
     for(int i = 0; i < NUM_ROUNDS; i++)
     {
