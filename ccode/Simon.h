04024c856b7c37646f5817354dc3821ea64f17ec
realiron00 <ic1595@naver.com>
2023-11-25 05:18:12 +0900

    2311250518

--- ccode/Simon.h
+++ ccode/Simon.h
@@ -11,8 +11,8 @@
 #include <stdint.h>
 #include <string.h>
 
-void simon_key_schedule(uint8_t* key, uint8_t* round_keys);
-void simon_encrypt(int round, uint8_t* round_keys, uint8_t* x, uint8_t* y);
+void simon_key_schedule(uint64_t* key, uint64_t* round_keys);
+void simon_encrypt(int round, uint64_t* round_keys, uint64_t* x, uint64_t* y);
 //0b10101111011100000011010010011000101000010001111110010110110011
 uint8_t z[WORD_SIZE-2] =
 {
